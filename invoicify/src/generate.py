604115ec87dac064f9acda35bcb3fc139b1e7bce
Cristian Mejia <cristianmejia97@gmail.com>
2021-12-13 22:06:33 -0400

    Refactor work calculation to make a better use of config.yml

--- invoicify/src/generate.py
+++ invoicify/src/generate.py
@@ -26,12 +26,18 @@ def seconds_to_hours(seconds):
     return seconds / 3600
 
 
+def get_child_duration(tag, report_group):
+    return [
+        child
+        for child in report_group.get("children")
+        if child.get("nameLowerCase") == tag
+    ][0].get("duration")
+
+
 def make_report(args, config):
     start_date = parse_date(args.start_date)
     end_date = parse_date(args.end_date)
     clockify_api_key = args.clockify_api_key or CLOCKIFY_API_KEY
-    dev_rate = config.get("variables").get("rates").get("dev")
-    coaching_rate = config.get("variables").get("rates").get("coaching")
 
     if not clockify_api_key:
         raise Exception("Missing Clockify API Key")
@@ -46,26 +52,35 @@ def make_report(args, config):
     LOG.debug("Got report data", extra={"report": report})
     report_group = report.get("groupOne")[0]
 
-    coaching_duration = [
-        child
-        for child in report_group.get("children")
-        if child.get("nameLowerCase") == "coaching"
-    ][0].get("duration")
-    duration_total = report_group.get("duration")
-    development_duration = duration_total - coaching_duration
+    report_tags = config.get("tags").keys()
+    tags_duration = {}
+    tags_cost = {}
 
-    LOG.debug(
-        "Calculated duration",
-        extra={
-            "coaching_duration": seconds_to_hours(coaching_duration),
-            "development_duration": seconds_to_hours(development_duration),
-        }
-    )
+    for tag in report_tags:
+        tag_rate = config.get("tags").get(tag).get("rate")
+        tags_duration[tag] = seconds_to_hours(get_child_duration(tag, report_group))
+        tags_cost[tag] = tag_rate * tags_duration[tag]
+        LOG.debug("Got tag duration", extra={"tag": tag, "duration": tags_duration[tag]})
+        LOG.debug("Got tag cost", extra={"tag": tag, "cost": tags_cost[tag]})
 
-    development_cost = dev_rate * seconds_to_hours(development_duration)
-    coaching_cost = coaching_rate * seconds_to_hours(coaching_duration)
+    duration_total = sum(tags_duration.values())
+    total_cost = sum(tags_cost.values())
+
+    work_details = []
 
     invoice_range = f"{format_date(start_date)} - {format_date(end_date)}"
+    for tag in report_tags:
+        tag_info = config.get("tags").get(tag)
+        tag_name = tag_info.get("description")
+        work_details.append(
+            {
+                "desc": f"{tag_name} {invoice_range}",
+                "time": round(tags_duration[tag], 2),
+                "cost": round(tags_cost[tag], 2),
+                "rate": tag_info.get("rate"),
+            }
+        )
+
     invoice_number = args.invoice_number or end_date.strftime("%Y%m%d")
 
     bank = config.get("variables").get("bank")
@@ -77,11 +92,9 @@ def make_report(args, config):
         {
             "invoice_number": invoice_number,
             "date_issue": datetime.today().strftime("%d/%m/%Y"),
-            "date_expire": add_n_months_to_date(end_date, 1).strftime(
-                "%d/%m/%Y"
-            ),
+            "date_expire": add_n_months_to_date(end_date, 1).strftime("%d/%m/%Y"),
             "currency": config.get("variables").get("currency"),
-            "total_time": round(seconds_to_hours(duration_total), 2),
+            "total_time": round(duration_total, 2),
             "client": {
                 "name": client.get("name"),
                 "address": client.get("address"),
@@ -94,21 +107,8 @@ def make_report(args, config):
                 "address": company.get("address"),
                 "phone": company.get("phone"),
             },
-            "work": [
-                {
-                    "desc": f"Development {invoice_range}",
-                    "time": round(seconds_to_hours(development_duration), 2),
-                    "rate": dev_rate,
-                    "cost": round(development_cost, 2),
-                },
-                {
-                    "desc": f"Coaching {invoice_range}",
-                    "time": round(seconds_to_hours(coaching_duration), 2),
-                    "rate": coaching_rate,
-                    "cost": round(coaching_cost, 2),
-                },
-            ],
-            "total_cost": round(development_cost + coaching_cost, 2),
+            "work": work_details,
+            "total_cost": round(total_cost, 2),
             "bank": {
                 "name": bank.get("name"),
                 "account_number": bank.get("account_number"),
--- readme.md
+++ readme.md
@@ -6,6 +6,7 @@ this is a simple clockify invoice generator.
 ## Installation
 `brew install wkhtmltopdf` or `apt-get install wkhtmltopdf` if linux based.
 `pip3 install -r requirements.txt`
+`cp config.yaml.example config.yaml`
 
 ## Usage
 python3 -m invoicify generate --config config.yaml
